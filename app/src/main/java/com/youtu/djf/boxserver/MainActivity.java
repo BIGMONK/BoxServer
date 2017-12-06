993f1fb6305a93a1df5c04456d854f2e9b06e44e
djf-ut-PC <488698112@qq.com>
2017-12-06 10:47:04 +0800

    服务端显示ip+mac，方便调试

--- app/src/main/java/com/youtu/djf/boxserver/MainActivity.java
+++ app/src/main/java/com/youtu/djf/boxserver/MainActivity.java
@@ -3,13 +3,22 @@ package com.youtu.djf.boxserver;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
+import android.widget.TextView;
+
+import com.djf.remotecontrol.DeviceUtils;
+import com.djf.remotecontrol.NetworkUtils;
+import com.djf.remotecontrol.RemoteUtils;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvIp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        tvIp = (TextView) findViewById(R.id.tv_ip);
+        tvIp.setText("MAC=" + DeviceUtils.getMacAddress()
+                + "  IP=" + NetworkUtils.getIPAddress(true));
     }
 
 
